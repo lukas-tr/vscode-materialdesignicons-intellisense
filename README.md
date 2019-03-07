99ce19e8fb26fa08f3b38682c07b0e8ca022463d
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2019-03-07 22:12:49 +0100

    basic functionality

--- README.md
+++ README.md
@@ -1,37 +1,37 @@
-# materialdesignicons-intellisense README
+# Material Design Icons Intellisense
 
-This is the README for your extension "materialdesignicons-intellisense". After writing up a brief description, we recommend including the following sections.
+Provides intellisense and hover preview of Material Design Icons.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Starts suggesting icon names after typing `mdi-`. Each entry contains a preview image and other information related to the icon, such as the icon category or aliases. 
 
-For example if there is an image subfolder under your extension project workspace:
+### Intellisense
 
-\!\[feature X\]\(images/feature-x.png\)
+![feature 1](doc/usage-1.gif)
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+### Hover
 
-## Requirements
+![feature 2](doc/usage-2.gif)
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+<!-- ## Requirements
+
+If you have any requirements or dependencies, add a section describing those and how to install and configure them. -->
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `materialdesigniconsIntellisense.iconColor`: RGB color of the preview icon
+- `materialdesigniconsIntellisense.iconSize`: Size of the preview icon in pixels
+- `materialdesigniconsIntellisense.selector`: Languages where completion and hover are active
+  - A list of identifiers can be found [here](https://code.visualstudio.com/docs/languages/identifiers) or when executing the `Change Language Mode` command.
 
-## Known Issues
+<!-- ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Calling out known issues can help limit users opening duplicate issues against your extension. -->
 
-## Release Notes
+<!-- ## Release Notes
 
 Users appreciate release notes as you update your extension.
 
@@ -45,21 +45,4 @@ Fixed issue #.
 
 ### 1.1.0
 
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Added features X, Y, and Z. -->
--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,191 @@
-// The module 'vscode' contains the VS Code extensibility API Import the module
-// and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// this method is called when your extension is activated your extension is
-// activated the very first time the command is executed
+const mdiPath = path.normalize(path.join(__dirname, '../node_modules/@mdi/svg/'));
+const mdiPackagePath = path.normalize(path.join(mdiPath, 'package.json'));
+const mdiMetaDataPath = path.normalize(path.join(mdiPath, 'meta.json'));
+
+interface IIconMeta {
+  id : string;
+  name : string;
+  codepoint : string;
+  aliases : string[];
+  tags : string[];
+  author : string;
+  version : string;
+}
+
+interface IIconDoc {
+  name : string;
+  codepoint : string;
+  aliases : string | null;
+  tags : string;
+  author : string;
+  version : string;
+  link : vscode.MarkdownString;
+  icon : vscode.MarkdownString;
+}
+
 export function activate(context : vscode.ExtensionContext) {
+  const getConfiguration = () => vscode
+    .workspace
+    .getConfiguration("materialdesigniconsIntellisense")
 
-  // Use the console to output diagnostic information (console.log) and errors
-  // (console.error) This line of code will only be executed once when your
-  // extension is activated
-  console.log('Congratulations, your extension "materialdesignicons-intellisense" is now active' +
-      '!');
-
-  // The command has been defined in the package.json file Now provide the
-  // implementation of the command with registerCommand The commandId parameter
-  // must match the command field in package.json
-  let disposable = vscode
-    .commands
-    .registerCommand('extension.helloWorld', () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode
-        .window
-        .showInformationMessage('Hello World asdf!');
+  const getMdiMetaData = () : Promise < IIconMeta[] > => new Promise((resolve, reject) => {
+    fs.readFile(mdiMetaDataPath, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      return resolve(JSON.parse(data.toString('utf8')));
     });
+  })
+
+  const encodeSpaces = (content : string) => {
+    return content.replace(/ /g, '%20');
+  }
+
+  const getIconData = (item : IIconMeta) : Promise < IIconDoc > => {
+
+    const svgPath = path.normalize(path.join(mdiPath, "svg", `${item.name}.svg`));
+    return new Promise((resolve, reject) => {
+      fs.readFile(svgPath, (err, data) => {
+        if (err) {
+          vscode
+            .window
+            .showErrorMessage(err.message);
+          return reject(err);
+        }
+        const iconSize = getConfiguration().get < number > ("iconSize") || 100;
+        const iconColor = getConfiguration().get < string > ("iconColor") || "#bababa";
+        const utf8String = data
+          .toString("utf8")
+          .replace(/<path/gi, `<path fill="${iconColor}" `);
+        const previewSvg = 'data:image/svg+xml;utf8;base64,' + Buffer
+          .from(utf8String)
+          .toString("base64") + encodeSpaces(` | width=${iconSize} height=${iconSize}`);
+        return resolve({
+          aliases: item
+            .aliases
+            .join(", ") || null,
+          author: item.author,
+          codepoint: item.codepoint,
+          name: item.name,
+          tags: item
+            .tags
+            .join(", ") || "Other",
+          version: item.version,
+          link: new vscode.MarkdownString(`[docs](https://materialdesignicons.com/icon/${item.name})`),
+          icon: new vscode.MarkdownString(`![preview](${previewSvg})`)
+        })
+      })
+    })
+  }
+
+  const showMdiVersionCommand = vscode
+    .commands
+    .registerCommand('extension.show-mdi-version', () => {
+      fs.readFile(mdiPackagePath, (err, data) => {
+        if (err) {
+          vscode
+            .window
+            .showErrorMessage(err.message);
+          return;
+        }
+        vscode
+          .window
+          .showInformationMessage('materialdesignicons-intellisense uses @mdi/svg@' + (JSON.parse(data.toString('utf8')))['version']);
+      })
+    });
+
+  const selector = getConfiguration().get < string[] > ("selector") || [];
+
+  const hoverProvider = vscode
+    .languages
+    .registerHoverProvider(selector, {
+      async provideHover(document : vscode.TextDocument, position : vscode.Position) {
+        const regex = /mdi-((\w|\-)+)/i;
+        const range = document.getWordRangeAtPosition(position, regex);
+        if (!range) {
+          return null;
+        }
+        const text = document.getText(range);
+
+        const match = regex.exec(text);
+        if (!match) {
+          return null;
+        }
+        const iconName = match[1];
+
+        const meta = await getMdiMetaData();
+
+        for (const item of meta) {
+          if (iconName == item.name) {
+            const meta = await getIconData(item);
+            const hover : vscode.Hover = {
+              range,
+              contents: [meta.icon, meta.tags, meta.link]
+            }
+            return hover;
+          }
+        }
+        const hover : vscode.Hover = {
+          range,
+          contents: [`icon ${iconName} not found`]
+        }
+        return hover;
+      }
+    })
+
+  const completionProvider = vscode
+    .languages
+    .registerCompletionItemProvider(selector, {
+      async provideCompletionItems(document : vscode.TextDocument, position : vscode.Position) {
+
+        let linePrefix = document
+          .lineAt(position)
+          .text
+          .substr(0, position.character);
+        if (!linePrefix.endsWith('mdi-')) {
+          return [];
+        }
+
+        const meta = await getMdiMetaData();
+
+        return [...meta].map((m) : vscode.CompletionItem & {
+          meta: any
+        } => {
+          return {label: `mdi-${m.name}`, kind: vscode.CompletionItemKind.Text, sortText: m.name, meta: m};
+        })
+      },
+      resolveCompletionItem(item : vscode.CompletionItem & {
+        meta: any
+      }, token : vscode.CancellationToken): vscode.ProviderResult < vscode.CompletionItem > {
+
+        return getIconData(item.meta).then(data => {
+          return ({
+            ...item,
+            documentation: data
+              .icon
+              .appendMarkdown(`
+- link: ${data.link.value}
+- aliases: ${data.aliases}
+- codepoint: ${data.codepoint}
+- author: ${data.author}
+- version: ${data.version}`),
+            detail: data.tags
+          })
+        });
+      }
+    }, "-");
 
   context
     .subscriptions
-    .push(disposable);
+    .push(showMdiVersionCommand, hoverProvider, completionProvider);
+
+  console.log('"materialdesignicons-intellisense" is now active');
+
 }
 
 // this method is called when your extension is deactivated
