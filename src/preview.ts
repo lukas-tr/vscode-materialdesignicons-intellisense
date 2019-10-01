15184c92d5683b72225eb519e6309647daa52616
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2019-10-01 13:47:48 +0200

    add Content-Security-Policy to webview

--- src/preview.ts
+++ src/preview.ts
@@ -38,6 +38,7 @@ export const showPreview = (
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline';">
           <title>Preview</title>
           <style>
             body {
